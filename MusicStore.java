3c724376d394c51474acae8b95ccc4926ee781fc
asgalan04 <147687449+asgalan04@users.noreply.github.com>
2025-02-28 13:17:14 -0700

    Add files via upload

---
+++ MusicStore.java
@@ -0,0 +1,101 @@
+package Library;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MusicStore {
+	private ArrayList<Album> albums;
+	
+	
+	public MusicStore() {
+		albums=new ArrayList<Album>();
+		ArrayList<File> curr=new ArrayList<File>();
+		try {
+			curr=processAlbums("albums.txt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		for (File file: curr) {
+			try {
+				readAlbumFile(file);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+			
+	}
+	
+	public Song searchSong(String songName) {
+		for(Album songs: albums) {
+			if(songs.getSong(songName)!=null) {
+				return songs.getSong(songName);
+			}
+		}
+		return null;
+	}
+
+	public ArrayList<Album> getAlbums(){
+		ArrayList<Album> deepCopy=new ArrayList<Album>();
+		for(Album album: albums) {
+			Album current=new Album(album.getAuthorName(),album.getAlbumName(),
+					album.getYear(),album.getGenre());
+			for(Song s: album.getSongs()) {
+				current.addSong(s);
+				
+			}
+			deepCopy.add(current);
+			
+		}
+		return deepCopy;
+	}
+	
+	//new object 
+	public Album searchAlbum(String albumName) {
+		for (Album album: albums) {
+			if(album.getAlbumName().toLowerCase().equals(albumName.toLowerCase())) {
+				Album current=new Album(album.getAuthorName(),album.getAlbumName(),
+						album.getYear(),album.getGenre());
+				for(Song s: album.getSongs()) {
+					current.addSong(s);
+					
+				}
+				return current;
+			}
+		}
+		return null;
+		
+	}
+	
+	public void readAlbumFile(File fileName) throws FileNotFoundException {
+		Scanner scan=new Scanner(fileName);
+		String [] attributes= scan.nextLine().split(",");
+		String albumTitle=attributes[0];
+		String Artist=attributes[1]; 
+		String genre=attributes[2];
+		int year=Integer.parseInt(attributes[3]);
+		Album current=new Album(Artist,albumTitle,year,genre);
+		while(scan.hasNextLine()) {
+			current.addSong(new Song(scan.nextLine().trim(),Artist,albumTitle));
+		}
+		
+		albums.add(current);
+		
+		
+	}
+	
+	public ArrayList<File> processAlbums(String fileName) throws FileNotFoundException {
+		ArrayList<File> fileNames=new ArrayList<File>();
+		File file=new File(fileName);
+		Scanner scan=new Scanner(file);
+		while(scan.hasNextLine()) {
+			String[] names=scan.nextLine().split(",");
+			fileNames.add(new File(names[0]+"_"+names[1]+".txt"));
+		}
+		return fileNames;
+		
+		
+	}
+	
+}
--- Song.java
+++ Song.java
@@ -2,16 +2,37 @@ package Library;
 
 public class Song {
 	private String songName;
+	private String authorName;
+	private String album;
 	private int rating;
+	private boolean favorite;
 
-	public Song(String songName) {
+	public Song(String songName,String authorName,String album) {
+		this.album=album;
+		this.authorName=authorName;
 		this.songName = songName;
 		this.rating = 0;
+		this.favorite=false;
 	}
+	
 
-	public void setFavorite() {
-		this.rating = 5;
 
+	public void setFavorite(boolean value) {
+		this.favorite = value;
+		
+
+	}
+	
+	public void setRating(int value) {
+		if(value==5) {
+			favorite=true;
+		}
+		rating=value;
+		
+		
+	}
+	public boolean isFavorite() {
+		return favorite;
 	}
 
 	public int getRating() {
@@ -22,10 +43,21 @@ public class Song {
 		return songName;
 
 	}
+	public String getAlbum() {
+		return album;
+	}
+	
+	public String getAuthor() {
+		return authorName;
+	}
 
 	public String toString() {
-		return songName;
+		return "Song name: "+songName+ "Author: "+authorName+"album name "+ album;
 
 	}
+
+
+
+	
 
 }
